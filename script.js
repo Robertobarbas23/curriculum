485741fa87ff711b618bece3b50903a0c595cd71
Robertobarbas23 <tandemrobertoaranjuez@gmail.com>
2024-03-13 11:30:43 +0100

    subo otra vez curriculum

--- script.js
+++ script.js
@@ -32,5 +32,32 @@ function irAInstagram() {
     var usuario = 'roberbarbas'; // Reemplaza con tu nombre de usuario de Instagram
     window.location.href = 'https://www.instagram.com/' + usuario;
 }
+document.querySelectorAll('.printbutton').forEach(function(element) {
+    element.addEventListener('click', function() {
+        print();
+    });
+  });
+  function mostrarSeccion(id) {
+    // Oculta todas las secciones
+    let secciones = document.querySelectorAll('section');
+    secciones.forEach(function(seccion) {
+        seccion.style.display = 'none';
+    });
 
+    // Muestra la sección con el ID dado
+    let seccionAMostrar = document.getElementById(id);
+    seccionAMostrar.style.display = 'block';
+}
+window.onload = function() {
+    mostrarSeccion('DatosPersonales');
+};
+function toggleNav() {
+    var navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active'); // Alternar la clase 'active' en el menú
+}
+  function imprimir() {
+    let secciones = document.querySelectorAll('section')
+    secciones.forEach(seccion=> seccion.style.display='block')
 
+   window.print();
+ }
